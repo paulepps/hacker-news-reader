e0523912b70521d47ef56265e0951dba0dc86f06
Paul Epps <paul@eppsnet.com>
2020-06-28 20:39:12 -0700

    Updates

---
+++ precache-manifest.3145209ecda5d7e24b8d791b336a7e4d.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b8b6198e8e4120bd658aff8e04f397ec",
+    "url": "/hacker-news-reader/index.html"
+  },
+  {
+    "revision": "156aea3890258252362c",
+    "url": "/hacker-news-reader/static/js/2.77568de4.chunk.js"
+  },
+  {
+    "revision": "863004d46cd7375f6c043c934b2ed4e3",
+    "url": "/hacker-news-reader/static/js/2.77568de4.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b96221d5bb08598110b1",
+    "url": "/hacker-news-reader/static/js/main.072b4b8e.chunk.js"
+  },
+  {
+    "revision": "ff4bc0fb6f75617eb8f5",
+    "url": "/hacker-news-reader/static/js/runtime-main.311226f2.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -11,13 +11,19 @@
  * See https://goo.gl/2aRDsh
  */
 
-importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
+importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/hacker-news-reader/precache-manifest.8c4fc3166c8c795cb8f3d94ecf44aa24.js"
+  "/hacker-news-reader/precache-manifest.3145209ecda5d7e24b8d791b336a7e4d.js"
 );
 
-workbox.clientsClaim();
+self.addEventListener('message', (event) => {
+  if (event.data && event.data.type === 'SKIP_WAITING') {
+    self.skipWaiting();
+  }
+});
+
+workbox.core.clientsClaim();
 
 /**
  * The workboxSW.precacheAndRoute() method efficiently caches and responds to
@@ -25,10 +31,9 @@ workbox.clientsClaim();
  * See https://goo.gl/S9QRab
  */
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
-workbox.precaching.suppressWarnings();
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute("/hacker-news-reader/index.html", {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/hacker-news-reader/index.html"), {
   
-  blacklist: [/^\/_/,/\/[^/]+\.[^/]+$/],
+  blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
